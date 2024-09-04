7f63ed37dc46a8e6e6e11907d01d948c938c7dc8
renatof4rias <renatof4rias@gmail.com>
2024-09-04 16:51:36 -0300

    initial commit

---
+++ src/oi.java
@@ -0,0 +1,15 @@
+public class oi {
+
+    public static int soma(int numero1, int numero2) {
+        return numero1 + numero2;
+    }
+
+    public static void main(String[] args) {
+
+        int result = oi.soma(10, 20);
+
+        System.out.println("soma = " + result);
+
+    }
+
+}
